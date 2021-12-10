e626758fda7dd88bbba5241c18f1d8633a7cc8a2
wsasouza <wsasouza@hotmail.com>
2021-12-09 21:51:09 -0300

    Feat: Implement CRUD Post

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,3 +1,14 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 import { Post } from '../entities/post.entity';
 
-export class CreatePostDto extends Post {}
+export class CreatePostDto extends Post {
+  @IsString()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  content: string;
+
+  @IsEmail()
+  authorEmail: string;
+}
--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,9 @@
-export class Post {}
+import { Prisma } from '@prisma/client';
+
+export class Post implements Prisma.PostUncheckedCreateInput {
+  id?: number;
+  title: string;
+  content?: string | null;
+  published?: boolean | null;
+  authorId?: number | null;
+}
